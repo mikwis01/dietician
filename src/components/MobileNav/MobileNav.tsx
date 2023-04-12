d82e3de2c4591cd70cff30e3e51efa561195b2cf
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2023-04-12 09:13:44 +0200

    feat: add mobile nav closing icon

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -1,5 +1,6 @@
 import { navLinks } from "@/constants/constants"
-import { motion } from "framer-motion"
+import { AnimatePresence, motion } from "framer-motion"
+import { VscChromeClose } from "react-icons/vsc"
 import Link from "next/link"
 const MobileNavVariants = {
   hidden: {
@@ -10,28 +11,57 @@ const MobileNavVariants = {
   }
 }
 
+const MobileNavItemsVariants = {
+  hidden: {
+    y: -100,
+    opacity: 0
+  },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+}
+
 export const MobileNav = () => {
   return (
     <div className="fixed h-screen w-screen flex items-center justify-center">
-      <motion.div
-        className="w-[300vw] h-[300vw] bg-gradient-to-b from-customGreen-light to-customGreen-semiLight rounded-[50%] flex items-center justify-center flex-none"
-        variants={MobileNavVariants}
-        initial={"hidden"}
-        animate={"visible"}
-        transition={{ duration: 0.5 }}>
-        <div className="fixed h-screen w-screen flex items-center justify-center">
-          <ul className="flex flex-col gap-4 text-center text-white">
-            {navLinks.map((item, i) => (
-              <motion.li
-                key={i}
-                whileHover={{ filter: "brightness(0.85)" }}
-                whileTap={{ scale: 0.9 }}>
-                <Link href={item.href}>{item.label}</Link>
-              </motion.li>
-            ))}
-          </ul>
-        </div>
-      </motion.div>
+      <AnimatePresence>
+        <motion.span
+          variants={MobileNavItemsVariants}
+          initial={"hidden"}
+          animate={"visible"}
+          exit={"hidden"}
+          transition={{ delay: 0.4 }}
+          className="absolute top-3 right-[8.333%]">
+          <VscChromeClose size={35} color="white" />
+        </motion.span>
+        <motion.div
+          className="w-[300vw] h-[300vw] bg-gradient-to-b from-customGreen-light to-customGreen-semiLight rounded-[50%] flex items-center justify-center flex-none"
+          variants={MobileNavVariants}
+          initial={"hidden"}
+          animate={"visible"}
+          exit={"hidden"}
+          transition={{ duration: 0.4 }}>
+          <div className="fixed h-screen w-screen flex flex-col items-center justify-center">
+            <motion.ul
+              className="flex flex-col gap-6 text-center text-white"
+              variants={MobileNavItemsVariants}
+              initial={"hidden"}
+              animate={"visible"}
+              exit={"hidden"}
+              transition={{ delay: 0.4 }}>
+              {navLinks.map((item, i) => (
+                <motion.li
+                  key={i}
+                  whileHover={{ filter: "brightness(0.85)" }}
+                  whileTap={{ scale: 0.9 }}>
+                  <Link href={item.href}>{item.label}</Link>
+                </motion.li>
+              ))}
+            </motion.ul>
+          </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
