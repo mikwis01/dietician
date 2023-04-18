8c04933c8e1697ff4dc56e891c969cc351c46601
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2023-04-19 01:56:47 +0200

    fix/perf: fix responsivness bugs and performance issues

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link"
 import { MobileNavHamburger } from "@/components/MobileNavHamburger/MobileNavHamburger"
 import Image from "next/image"
-import Logo from "../../assets/MultiDiet.png"
+import Logo from "../../assets/MultiDiet.webp"
 import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { navLinks } from "@/constants/constants"
 
@@ -13,9 +13,9 @@ export const Nav = () => {
       <div className="w-5/6 h-full flex items-center justify-between">
         <Image src={Logo} alt={"Dietetyk Marta Zarazińska"} className="w-28" />
         {isDesktop ? (
-          <ul className="flex gap-8 lg:text-white lg:gap-12">
+          <ul className="flex gap-8 lg:text-white lg:gap-6 xl:gap-12">
             {navLinks.slice(1).map((item, i) => (
-              <li key={i} className="font-bold">
+              <li key={i} className="font-semibold">
                 <Link href={item.href}>{item.label}</Link>
               </li>
             ))}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import { Layout } from "@/components/Layout/Layout"
 import Image from "next/image"
-import MobileBacground from "../assets/MobileBackground.png"
-import MobileBacgroundPattern from "../assets/MobileBackgroundPattern.png"
-import DesktopBackground from "../assets/DesktopBackground.png"
-import Dietician from "../assets/Dietician.png"
+import MobileBacground from "../assets/MobileBackground.webp"
+import MobileBacgroundPattern from "../assets/MobileBackgroundPattern.webp"
+import DesktopBackground from "../assets/DesktopBackground.webp"
+import Dietician from "../assets/Dietician.webp"
 import Link from "next/link"
 import { motion } from "framer-motion"
 
@@ -12,25 +12,25 @@ export default function Home() {
     <Layout>
       <article className="flex items-center justify-center min-h-screen marker:lg:flex-row">
         <div className="flex flex-col items-center min-h-screen justify-between pt-16 lg:flex-row lg:w-5/6">
-          <section className="flex-1 w-5/6 flex flex-col justify-center items-center gap-8 my-8 min-[500px]:my-16 min-[500px]:text-center lg:w-1/2 lg:text-start lg:gap-12 lg:items-start">
-            <h1 className="font-black text-xl w-full md:text-2xl md:w-4/5  xl:text-3xl">
+          <section className="flex-1 w-5/6 flex flex-col justify-center items-center gap-8 my-8 min-[500px]:my-16 min-[500px]:text-center lg:w-1/2 lg:text-start lg:gap-12 lg:items-start 2xl:gap-16">
+            <h1 className="font-black text-xl w-full md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl">
               Problemy ze zdrowiem i sylwetką nie muszą być już Twoim zmartwieniem!
             </h1>
-            <h2 className="opacity-80 w-full md:text-lg md:w-4/5 lg:text-xl">
-              Pomogę Ci stać się lepszą wersją siebie dzięki zdrowszemu żywieniu.
+            <h2 className="opacity-80 w-full md:text-lg lg:text-xl 2xl:text-2xl">
+              Pomogę Ci stać się lepszą wersją siebie dzięki<br></br> zdrowszemu żywieniu.
             </h2>
-            <ul className="w-full flex gap-5 text-sm font-bold text-center min-[500px]:justify-center lg:justify-start lg:text-lg">
+            <ul className="w-full flex gap-5 text-sm font-bold text-center min-[500px]:justify-center lg:justify-start xl:text-lg">
               <motion.li
-                className="w-1/2 max-w-[12.5rem] h-12 bg-customGreen-button rounded-md text-white px-2 lg:h-14 lg:max-w-[15rem]"
+                className="w-1/2 max-w-[12.5rem] h-12 bg-customGreen-button rounded-md text-white px-2 xl:h-14 xl:max-w-[15rem]"
                 whileTap={{ scale: 0.9 }}>
                 <Link href={"/o-mnie"} className="w-full h-full grid place-items-center">
                   Poznaj mnie lepiej
                 </Link>
               </motion.li>
               <motion.li
-                className="w-1/2 max-w-[12.5rem] h-12 border-customGreen-button border-[3px] rounded-md text-customGreen-semiLight px-2 lg:h-14 lg:max-w-[15rem]"
+                className="w-1/2 max-w-[12.5rem] h-12 border-customGreen-button border-[3px] rounded-md text-customGreen-semiLight px-2 xl:h-14 xl:max-w-[15rem]"
                 whileTap={{ scale: 0.9 }}>
-                <Link href={"/oferta"} className="w-full h-full grid place-items-center">
+                <Link href={"/oferta"} className="w-full h-full grid place-items-center ">
                   Sprawdź moją ofertę
                 </Link>
               </motion.li>
@@ -51,7 +51,11 @@ export default function Home() {
           </section>
           <div className="hidden absolute lg:block w-1/2 h-full right-0 top-0">
             <Image src={DesktopBackground} alt="dasdsa" className="h-full w-4/5 right-0 absolute" />
-            <Image src={Dietician} alt="dasdas" className="absolute bottom-0 w-11/12" />
+            <Image
+              src={Dietician}
+              alt="dasdas"
+              className="absolute bottom-0 right-0 w-11/12 max-w-6xl"
+            />
           </div>
         </div>
       </article>
