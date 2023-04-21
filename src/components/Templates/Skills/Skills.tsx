154cd5fc8f4a11ee5e450630903e20211ad8fd0b
Mikołaj Wiśniewski <javadreamsmikolaj@gmail.com>
2023-04-21 20:59:17 +0200

    fix: fix background color on skill page

--- src/components/Templates/Skills/Skills.tsx
+++ src/components/Templates/Skills/Skills.tsx
@@ -5,7 +5,7 @@ import React from "react"
 export const Skills = () => {
   return (
     <article className="flex items-center py-8 justify-center lg:min-h-[calc(832px+4rem)]">
-      <div className="flex flex-col items-center h-[60vh] gap-6 lg:flex-row-reverse lg:w-5/6 lg:min-h-[832px]">
+      <div className="flex flex-col items-center gap-6 lg:flex-row-reverse lg:w-5/6 lg:min-h-[832px]">
         <div className="flex flex-col items-center gap-6 lg:w-2/3 lg:min-h-[832px]">
           <Header
             text={
